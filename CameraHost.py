add0113fde941911df5caefdea09e924821eb490
agniaendie <endienasg3@gmail.com>
2024-06-15 07:41:16 +0500

    fix camera registry

--- CameraHost.py
+++ CameraHost.py
@@ -1,4 +1,5 @@
 import random
+import uuid
 
 import cv2
 import requests
@@ -26,11 +27,17 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    server_addr = "localhost:30081"
-    app.run(host='0.0.0.0', port=8080, debug=True)
+
+    server_addr = "http://localhost:30081/registry"
+    local_uuid = uuid.uuid4()
     IP = requests.get('https://api.ipify.org/').text
     ip_port = f'{IP}:{8080}/video_capture'
-    requests.post(server_addr, json={
+    co = requests.post(server_addr, json={
         "name": f"foxwatch{random.randint(0,100)}",
-        "ip": ip_port
+        "ip": ip_port,
+        "uuid":str(local_uuid)
     })
+    print(co.json())
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+    requests.delete(f'http://localhost:30081/remove/{local_uuid}')
