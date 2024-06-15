581c2a56abf679accf0d310faf6535eace0821e4
agniaendie <endienasg3@gmail.com>
2024-06-15 06:23:55 +0500

    add registry to camera

--- CameraHost.py
+++ CameraHost.py
@@ -1,4 +1,7 @@
+import random
+
 import cv2
+import requests
 from flask import Flask, Response
 
 app = Flask(__name__)
@@ -17,10 +20,17 @@ def generate_frames():
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route('/video_feed')
+@app.route('/video_capture')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
+    server_addr = "localhost:30081"
     app.run(host='0.0.0.0', port=8080, debug=True)
+    IP = requests.get('https://api.ipify.org/').text
+    ip_port = f'{IP}:{8080}/video_capture'
+    requests.post(server_addr, json={
+        "name": f"foxwatch{random.randint(0,100)}",
+        "ip": ip_port
+    })
